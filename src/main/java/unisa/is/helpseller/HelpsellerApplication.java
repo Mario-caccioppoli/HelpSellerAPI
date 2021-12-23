70d11e9e56d912bb5cda3b4a9266bd9d8b4c0cd0
Daniele Iervolino <Daniele Iervolino@DESKTOP-NV0N19H>
2021-12-23 14:51:26 +0100

    Created the git repo and first commit on Github

---
+++ src/main/java/unisa/is/helpseller/HelpsellerApplication.java
@@ -0,0 +1,14 @@
+// Default File creato con Spring Initializr
+package unisa.is.helpseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpsellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpsellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unisa/is/helpseller/HelpsellerApplicationTests.java
@@ -0,0 +1,13 @@
+package unisa.is.helpseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpsellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
