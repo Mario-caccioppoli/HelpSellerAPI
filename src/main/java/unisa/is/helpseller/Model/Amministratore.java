19f436062a3a7269bd675d9b57147666ee35e1ad
Daniele Iervolino <Daniele Iervolino@DESKTOP-NV0N19H>
2022-01-03 17:24:25 +0100

    Modified Amministratore model

--- src/main/java/unisa/is/helpseller/Model/Amministratore.java
+++ src/main/java/unisa/is/helpseller/Model/Amministratore.java
@@ -1,31 +1,19 @@
 package unisa.is.helpseller.Model;
 
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
-import javax.persistence.Id;
+import java.io.Serializable;
+import javax.persistence.*;
 
 
 @Entity
 @Table(name = "amministratore")
-public class Amministratore {
+public class Amministratore implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     
-    @Column
-    private int idAmministratore;
-    
-    @Column
     private String username;
-    
-    @Column
     private String email;
-    
-    @Column
     private String password;
     
     public Amministratore() {}
@@ -35,5 +23,33 @@ public class Amministratore {
         this.username = username;
         this.password = password;
     }
-    
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+       
 }
