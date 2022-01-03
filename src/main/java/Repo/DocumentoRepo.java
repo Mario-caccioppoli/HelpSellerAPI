7cd0915039d2d2fffcf837cbc3b742915b6535b3
UTENTE <UTENTE@DESKTOP-EB2L7O4>
2022-01-03 17:56:10 +0100

    Creato package Repo

---
+++ src/main/java/Repo/DocumentoRepo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import unisa.is.helpseller.Model.Documento;
+
+/**
+ *
+ * @author UTENTE
+ */
+public interface DocumentoRepo extends JpaRepository<Documento, Long>{
+    
+}
---
+++ src/main/java/Repo/ScontoProdottoRepo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import unisa.is.helpseller.Model.ScontoProdotto;
+
+/**
+ *
+ * @author UTENTE
+ */
+public interface ScontoProdottoRepo extends JpaRepository<ScontoProdotto, Long>{
+    
+}
