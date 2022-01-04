ffd61ba2892b3f2ef39d3f18e51ee3c6a81cd755
daniele <daniele@AsusElementaryOS>
2022-01-04 18:24:23 +0100

    Added serialization

--- src/main/java/unisa/is/helpseller/Entity/Trasporto.java
+++ src/main/java/unisa/is/helpseller/Entity/Trasporto.java
@@ -1,18 +1,14 @@
 package unisa.is.helpseller.Entity;
 
 
+import java.io.Serializable;
 import java.sql.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 @Entity
 @Table(name = "trasporto")
-public class Trasporto {
+public class Trasporto implements Serializable{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
