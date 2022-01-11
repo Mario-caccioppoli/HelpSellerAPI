000922ce119731873a8d89ce88394317d4ddf760
daniele <daniele@AsusElementaryOS>
2022-01-11 13:33:46 +0100

    Updated DB Schema (auto_increment recensione)

--- db.sql
+++ db.sql
@@ -247,7 +247,7 @@ DROP TABLE IF EXISTS `recensione`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `recensione` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `testo` varchar(255) DEFAULT NULL,
   `voto` int DEFAULT NULL,
   `data` date DEFAULT NULL,
